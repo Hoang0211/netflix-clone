39a3334a3d95d9b1344586298bce6284189fab1c
Nguyen Hoang <n3verland0211@gmail.com>
2022-05-10 18:27:32 +0300

    reorder imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import { FirebaseContextProvider } from './store/firebase-context';
 
-import './index.scss';
 import App from './App';
+import { FirebaseContextProvider } from './store/firebase-context';
+import './index.scss';
 
 ReactDOM.render(
   <FirebaseContextProvider>
